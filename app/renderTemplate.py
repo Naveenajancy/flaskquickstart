d047367c231119bbe42e44362acc5c6b491395ed
nkb05 <kb.jancy90@gmail.com>
2021-06-24 01:56:43 -0400

    update cookies - renderTemplate

--- app/renderTemplate.py
+++ app/renderTemplate.py
@@ -8,3 +8,9 @@ from flask import render_template
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
+
+@app.route('/')
+def index():
+    resp = make_response(render_template(...))
+    resp.set_cookie('username', 'the username')
+    return resp
